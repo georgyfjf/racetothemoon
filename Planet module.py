ac946f7a87414e418631ce3e558ee8b97d2de0f6
Egor Kurganov <egor_kurganov@hotmail.com>
2019-05-06 19:28:34 +0300

    make it work

--- Planet module.py
+++ Planet module.py
@@ -25,12 +25,19 @@ def vect_sum(r1, ang1, r2, ang2):
             ang = 3 / 2 * math.pi
         if y == 0:
             ang = None
-    return tuple(r, ang)
+    return tuple([r, ang])
 
 
 class Planet:
 
     def __init__(self, r, ang, m, R, v):
+        '''
+        :param r: first coordinate
+        :param ang: second coordinate
+        :param m: mass
+        :param R: radius
+        :param v: angular velocity
+        '''
         self.m = m
         self.r0 = (r, ang)
         self.radius = R
@@ -38,12 +45,15 @@ class Planet:
 
     def coord(self, t):  # Returns current coordinate
         if self.r0[1] is not None:
-            return tuple(self.r0[0], self.r0[1] + (t * self.v) % (2 * math.pi))
+            return tuple([self.r0[0], self.r0[1] + (t * self.v) % (2 * math.pi)])
+        else:
+            return self.r0
 
     def dist_surf(self, r, ang, t):  # Returns distance to the surface
         r1 = self.coord(t)
         if self.r0[1] is not None:
-            return vect_sum(r, ang, r1[0], r1[1])[0] - self.radius
+            print(vect_sum(r, ang, r1[0], (r1[1] + math.pi) % (2 * math.pi)))
+            return vect_sum(r, ang, r1[0], (r1[1] + math.pi) % (2 * math.pi))[0] - self.radius
         else:
             return r - self.radius
 
@@ -51,18 +61,19 @@ class Planet:
         a, b = self.coord(t)
         R = self.dist_surf(r, ang, t) + self.radius
         if self.r0[1] is not None:
-            return tuple((G * self.m) / R ** 2, vect_sum(r, ang, a, b)[1])
+            return tuple([(G * self.m) / R ** 2, vect_sum(r, (ang + math.pi) % (2 * math.pi), a, b)[1]])
         else:
-            return tuple((G * self.m) / R ** 2, ang)
+            return tuple([(G * self.m) / R ** 2, (ang + math.pi) % (2 * math.pi)])
 
     def atm_pres(self):  # coming soon
         pass
 
 
 Earth = Planet(0, None, 5.9726 * 10 ** 24, 6371000, 0)
-Moon = Planet(384467, 0, 7.3477 * 10 ** 22, 1737100, 2 * math.pi / (27 * 24 * 60 * 60))
+Moon = Planet(384467000, 0, 7.3477 * 10 ** 22, 1737100, 2 * math.pi / (27 * 24 * 60 * 60))
 
 
+print(Earth.gravity(Earth.radius, 2, 0))
 
 
 
